7595b33605fd5c6cc63bd035b7a39d2f49048ae5
Elyas97 <ilyaas.addawe@gmail.com>
2020-12-04 18:39:19 +0200

    Toimii kaikki

--- Loppuprojekti/js/main.js
+++ Loppuprojekti/js/main.js
@@ -17,6 +17,10 @@ button.addEventListener("click",function () {
         return;
         //jos tyhjä ei suoriteta koodia
     }
+    if (maara.value<0){
+        alert("syötä positiivinen luku")
+        return;
+    }
     if (tyyppi.value =="tulo"){
         let tulot ={
             type : "tulo",
@@ -35,12 +39,6 @@ button.addEventListener("click",function () {
         tyhjennaSyote()
 
     }
-    let kertynytTulo=summa("tulo",taulu);
-    let kertynytMeno=summa("meno",taulu);
-    let saldosi=(kertynytTulo-kertynytMeno);
-    saldoNyt.innerText=saldosi;
-    tulot.innerText=kertynytTulo;
-    menot.innerText=kertynytMeno
     naytolleLista(taulu)
 
 })
@@ -55,19 +53,29 @@ function summa(tyyppi ,taulu){
  return sum
 }
 
-// yrittäkää saamaan tämä toimimaan
-//kaikki tiedot on arrayssa loop sitten laitetaan näyttöön
+
 function naytolleLista(taulu){
+    //lasketaan saldo
+    let kertynytTulo=summa("tulo",taulu);
+    let kertynytMeno=summa("meno",taulu);
+    let saldosi=(kertynytTulo-kertynytMeno);
+    //näytölle
+    saldoNyt.innerText=saldosi;
+    tulot.innerText=kertynytTulo;
+    menot.innerText=kertynytMeno
+    //tyhjennetään vanha lista ja uusi
+    tuloLista.innerHTML="";
+    menoLista.innerHTML="";
         for (let i=0;i<taulu.length;i++) {
             let x=taulu[i]
             if (taulu[i].type == "tulo") {
             tuloLista.innerHTML+=
-                `<li id="${i}" class="testit"> <figure class="alkiolista">${x.maara}<figcaption>${x.tieto}</figcaption>
-                <button class="poista">poista</button> </figure></li>`
+                `<li id="${i}" class="listaTulo"> <figure class="alkiolista">${x.tieto}</figure>
+                    <p>${x.maara}<button class="poista">poista</button></p></li>`
             } else if(taulu[i].type == "meno"){
                 menoLista.innerHTML+=
-                    `<li id="${i}" class="testit"> <figure class="alkiolista">${x.maara}<figcaption>${x.tieto}</figcaption>
-                <button class="poista">poista</button> </figure></li>`
+                    `<li id="${i}" class="listaMeno"> <figure class="alkiolista">${x.tieto}</figure>
+                      <p>${x.maara} <button class="poista2">poista</button></p></li>`
             }
 }}
 
@@ -75,4 +83,20 @@ function tyhjennaSyote(){
     maara.value="";
     tieto.value="";
 }
-console.log(taulu)
+
+function poistaElementti (index,array){
+    array.splice(index,1)
+}
+//julkinen event listerner kaikille poista buttoneille
+addEventListener('click',function (e) {
+    let kohde=e.target;
+    let käynti=kohde.parentNode.parentNode;
+    if (kohde.className==="poista"){
+       poistaElementti(käynti.id,taulu)
+    }else if (kohde.className==="poista2"){
+     poistaElementti(käynti.id,taulu)
+    }
+    naytolleLista(taulu)
+
+})
+
