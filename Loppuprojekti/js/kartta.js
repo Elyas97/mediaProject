c4c64571ef427a1827f456c32ec2e05983572055
joelrau <71751328+j0elr4u@users.noreply.github.com>
2020-12-09 13:02:23 +0200

    koodin validointi ja indent

--- Loppuprojekti/js/kartta.js
+++ Loppuprojekti/js/kartta.js
@@ -1,6 +1,8 @@
+"use strict";
+
 const map = createMap(coordinates(60.17, 24.90), 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 addMarker(coordinates(60.1740, 24.9081), "<b>Toimipiste!</b><br>Terve!");
@@ -10,48 +12,48 @@ paikanna();
 
 // Tekee kartan ja palauttaa sen.
 function createMap(crd, zoom) {
-	return L.map('mapid').setView([crd.latitude, crd.longitude], zoom);
+    return L.map('mapid').setView([crd.latitude, crd.longitude], zoom);
 }
 
 // Lisää karttaan markkerin.
 function addMarker(crd, text) {
-	let marker = L.marker([crd.latitude, crd.longitude]).addTo(map);
-	marker.bindPopup(text);
+    let marker = L.marker([crd.latitude, crd.longitude]).addTo(map);
+    marker.bindPopup(text);
 }
 
 // Functio, joka palauttaa koordinaattit muuttujana.
 function coordinates(arg1, arg2) {
-	let kooridnaatti = {
-		latitude: arg1,
-		longitude: arg2
-	};
-	return kooridnaatti;
+    let kooridnaatti = {
+        latitude: arg1,
+        longitude: arg2
+    };
+    return kooridnaatti;
 }
 
 // Sijainnin löytyessä kertoo about missä olet.
 function onLocationFound(e) {
-	var radius = e.accuracy / 2;
-	L.marker(e.latlng).addTo(map)
-		.bindPopup("You are within " + radius + " meters from this point").openPopup();
-	L.circle(e.latlng, radius).addTo(map);
+    var radius = e.accuracy / 2;
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+    L.circle(e.latlng, radius).addTo(map);
 }
 
 // Functio, joka päivittää kartan näyttämään tietyn sijainnin
 function paivitaKartta(crd, zoom) {
-	// Käytetään leaflet.js -kirjastoa näyttämään sijainti kartalla (https://leafletjs.com/)
-	map.setView([crd.latitude, crd.longitude], zoom);
+    // Käytetään leaflet.js -kirjastoa näyttämään sijainti kartalla (https://leafletjs.com/)
+    map.setView([crd.latitude, crd.longitude], zoom);
 }
 
 // Funktio, joka ajetaan, jos paikkatietojen hakemisessa tapahtuu virhe
 function error(err) {
-	console.warn(`ERROR(${err.code}): ${err.message}`);
+    console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
 // Paikantaa oman sijainnin.
 function paikanna() {
-	map.locate({
-		setView: true,
-		maxZoom: 13,
-		watch: false
-	});
+    map.locate({
+        setView: true,
+        maxZoom: 13,
+        watch: false
+    });
 }
--- Loppuprojekti/js/kotisivu.js
+++ Loppuprojekti/js/kotisivu.js
@@ -1,28 +1,30 @@
+"use strict";
+
 const button = document.getElementById("generate_joke");
 button.addEventListener("click", generateJoke)
 
 // Funktio generoi vitsin ja tulostaa sen diviin.
 function generateJoke() {
-	const request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
 
-	request.open('GET', 'https://api.icndb.com/jokes/random');
-	request.send();
+    request.open('GET', 'https://api.icndb.com/jokes/random');
+    request.send();
 
-	request.onload = () => {
-		if (request.status === 200) {
-			//console.log("Success"); 
+    request.onload = () => {
+        if (request.status === 200) {
+            //console.log("Success"); 
 
-			//Extracting data
-			let joke = JSON.parse(request.response).value.joke;
-			//console.log(joke);
+            //Extracting data
+            let joke = JSON.parse(request.response).value.joke;
+            //console.log(joke);
 
-			//Showing the joke in the table
-			document.getElementById("joke").innerHTML = joke;
-		}
-	};
+            //Showing the joke in the table
+            document.getElementById("joke").innerHTML = joke;
+        }
+    };
 
-	request.onerror = () => {
-		console.log("ERROR");
-		document.getElementById("joke").innerHTML = "ERROR!<br>Check the console.";
-	}
+    request.onerror = () => {
+        console.log("ERROR");
+        document.getElementById("joke").innerHTML = "ERROR!<br>Check the console.";
+    }
 }
